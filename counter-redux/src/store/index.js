a8112b4946a2ddf998ab7580bbc2ec7a7fb4cd24
Pratik-Chavan-01 <pratik.c@codearray.tech>
2023-12-26 15:39:32 +0530

    Completed with redux toolkit

--- counter-redux/src/store/index.js
+++ counter-redux/src/store/index.js
@@ -1,25 +1,9 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import counterSlice from "./counter";
+import privacySlice from "./privacy";
 
-const INITIAL_VALUE = { counter: 0 };
-
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  let newStore = store;
-  if (action.type === "INCREMENT") {
-    newStore = { ...store, counter: store.counter + 1 };
-    return newStore;
-  } else if (action.type === "DECREMENT") {
-    newStore = { ...store, counter: store.counter - 1 };
-    return newStore;
-  } else if (action.type === "ADD") {
-    return { ...store, counter: store.counter + Number(action.payload.num) };
-  } else if (action.type === "SUBTRACT") {
-    return { ...store, counter: store.counter - Number(action.payload.num) };
-  } else if (action.type === "PRIVACY") {
-    return { ...store, privacy: !store.privacy };
-  }
-  return store;
-};
-
-const counterStore = createStore(counterReducer);
+const counterStore = configureStore({
+  reducer: { counter: counterSlice.reducer, privacy: privacySlice.reducer },
+});
 
 export default counterStore;
